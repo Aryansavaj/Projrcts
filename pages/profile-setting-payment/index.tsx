3db9c9c482aa9882647805778bb768e126e2dfe9
Aryansavaj <126572722+Aryansavaj@users.noreply.github.com>
2023-06-13 18:58:12 +0530

    wd

--- pages/profile-setting-payment/index.tsx
+++ pages/profile-setting-payment/index.tsx
@@ -49,7 +49,7 @@ export default function Profilepayment({}: Props) {
       </section>
 
       <section className="grid grid-cols-3 gap-6 profile_grid_set">
-        <div className="p-8 lg:p-8 bg-base-100 rounded-[3rem] gap-8 col-span-2 profile_grid_inner_set_1">
+        <div className="p-8 lg:p-8 bg-base-100 rounded-[28px] gap-8 col-span-2 profile_grid_inner_set_1">
           <ScrollReveal>
             <Title
               blackText=""
@@ -61,52 +61,68 @@ export default function Profilepayment({}: Props) {
         </div>
 
 
-        <div className="p-8 lg:p-8 bg-primary rounded-[3rem] gap-8 profile_grid_inner_set_2">
+        <div className="p-8 lg:p-8 bg-primary rounded-[28px] gap-8 profile_grid_inner_set_2">
           <ScrollReveal>
             <Title
               blackText={"Din Kundvagn"}
               blueText=""
-              className="text-left text-26 text-white"
+              className="text-left text-26 text-white title-font-size"
             />
           </ScrollReveal>
           <section className="relative mb-28">
             <div className="grid grid-cols-1 gap-6 mb-20">
-              <div className="bg-glass-light p-3 rounded-2xl">
+            <div className="bg-glass-light p-3 rounded-2xl">
                 <div className="flex justify-between items-center bg-base-200 py-4 px-4 hover:bg-base-200 rounded-2xl job-openings-shadow">
                   <Link key="" href="">
-                    <div className="mega-menu-list items-center flex gap-3  rounded-lg">
+                    <div className="mega-menu-list items-center flex gap-4 items-start  rounded-lg">
                       <div className="list-icon bg-base-200 rounded-lg">
                         <Image
                           src="/icons/pricing/2-premium.png"
                           alt="Mega menu icon"
                           className="object-contain"
-                          width={40}
-                          height={40}
+                          width={55}
+                          height={55}
                           priority
                         />
                       </div>
 
                       <div className="menu-content">
-                        <h2 className="font-semibold text-left">Checkified</h2>
-                        <h3 className="font-semibold text-left text-[14px]">
-                          14 997 :-
-                        </h3>
+                        <h2 className="font-semibold text-left text-[14px]">Checkified</h2>
+                        <h3 className="text-left text-[11px] font-medium">14 997 :-</h3>
                       </div>
 
                       <div>
                         <button
                           type="button"
-                          className="btn mr-2 w-full btn-primary  hover:bg-gray-100 hover:border-gray-200 items-center isolate"
+                          className="btn mr-2  w-full btn-primary items-center px-3 py-1 relative overflow-hidden"
                         >
-                          
-                        
+                          <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute absolute_obje_position">
+                            <path d="M30.1011 16.0353C30.1011 23.7779 23.7942 30.0662 15.9998 30.0662C8.20542 30.0662 1.89844 23.7779 1.89844 16.0353C1.89844 8.29263 8.20542 2.00439 15.9998 2.00439C23.7942 2.00439 30.1011 8.29263 30.1011 16.0353Z" stroke="url(#paint0_linear_1_2)" stroke-width="3"/>
+                            <defs>
+                            <linearGradient id="paint0_linear_1_2" x1="15.9998" y1="-16.4602" x2="15.9998" y2="24.987" gradientUnits="userSpaceOnUse">
+                            <stop stop-color="white"/>
+                            <stop offset="1" stop-color="white" stop-opacity="0"/>
+                            </linearGradient>
+                            </defs>
+                          </svg>
+                          <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute absolute_obje_position2">
+                                <ellipse cx="15.6013" cy="15.5309" rx="15.6013" ry="15.5309" fill="url(#paint0_linear_2_3)"/>
+                                <defs>
+                                <linearGradient id="paint0_linear_2_3" x1="18.2435" y1="45.5719" x2="18.2435" y2="7.36134" gradientUnits="userSpaceOnUse">
+                                <stop stop-color="white"/>
+                                <stop offset="0.935323" stop-color="white" stop-opacity="0"/>
+                                </linearGradient>
+                                </defs>
+                          </svg>
+
+
                           <div className="flex justify-center items-center w-full">
-                            <div className="mr-1 text-white font-semibold font-sans relative xs:text-sm">
+                            <div className="mr-1 text-white font-semibold font-sans relative text-[12px]">
                               Premium
                             </div>
                             <Image
                               src="/icons/profile-setting/chargeicon-1.png"
-                              className="relative border p-1 border-white rounded-full drop-shadow-xl"
+                              className="relative border buttton_img_pro_setting border-white rounded-full"
                               width="32"
                               height="32"
                               alt="Facebook"
@@ -120,32 +136,38 @@ export default function Profilepayment({}: Props) {
                 </div>
               </div>
 
-              <div className="bg-glass-border p-3 rounded-2xl">
-                <div
+              <div className="bg-glass-border p-2 rounded-2xl">
+              <div
                   key=""
                   className="flex justify-between items-center py-2 px-2 rounded-2xl"
                 >
-                  <div className="list-icon rounded-lg bg-transparent">
-                    <h2 className="font-semibold text-white">Totalt</h2>
-                  </div>
+                  
+                    <div className="list-icon rounded-lg bg-transparent">
+                      <h2 className="font-semibold text-white pro_setting_side_font">Moms</h2>
+                    </div>
 
-                  <div>
-                    <h2 className="font-semibold text-white">14 997 :- </h2>
-                  </div>
+                    <div>
+                    <h2 className=" text-white pro_setting_side_font font-bold">14 997 :- </h2>
+                      
+                    </div>
+                  
                 </div>
                 <div
                   key=""
                   className="flex justify-between items-center bg-white-gradient py-2 px-2 hover:bg-base-200 rounded-2xl"
                 >
-                  <div className="list-icon rounded-lg bg-transparent">
-                    <h2 className="font-semibold text-primary">Totalt</h2>
-                  </div>
+                  
+                    <div className="list-icon rounded-lg bg-transparent">
+                      <h2 className="font-semibold text-primary text-sm">Totalt</h2>
+                    </div>
 
-                  <div>
-                    <button className="btn hover:text-primary hover:border-primary btn-white">
-                      14 997 :-
+                    <div>
+                    <button className="btn hover:text-primary hover:border-primary btn-white text-[14px] py-2 px-6 pro_pri_btn font-bold">
+                    14 997 :-
                     </button>
-                  </div>
+                      
+                    </div>
+                  
                 </div>
               </div>
             </div>
