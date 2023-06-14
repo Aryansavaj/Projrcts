0fe87936d955aac856f23bbb0f223f2fdd2dc458
Aryansavaj <126572722+Aryansavaj@users.noreply.github.com>
2023-06-14 16:30:58 +0530

    dawd

--- components/AdminLayout.tsx
+++ components/AdminLayout.tsx
@@ -20,6 +20,20 @@ export default function Admin_layout({ headTitle, isFullWidth, children }: Props
       <div data-theme="light" className="overflow-hidden relative">
         <Admin_header setOpenSidebar={setOpenSidebar} openSidebar={openSidebar} />
         <main className="pt-header-height bg-base-100">
+            <div className="bg-primary">
+                <h1 class="Admin2_title">Manage Your Course</h1>
+                <div className="admin2_nav_button">
+                    <button className="btn hover:bg-base-200 bg-white opacity-[.14] w-full lg:w-fit mb-8 text-subtitle border-none font-medium text-sm pt-0 pb-0 btn-height-rem">
+                        Alla
+                    </button>
+                    <button className="btn btn-primary w-full lg:w-fit mb-8 ml-2 mr-2 text-sm font-medium pt-0 pb-0 btn-height-rem">
+                        Pågående
+                    </button>
+                    <button className="btn bg-base-200 hover:bg-base-200 w-full lg:w-fit mb-8 text-subtitle border-none text-sm font-medium pt-0 pb-0 btn-height-rem">
+                        Avslutad
+                    </button>
+                </div>
+            </div>
           <div className="p-8 lg:p-12 bg-base-200 flex flex-col lg:flex-row gap-8 height-fullscreen bg_color_dash">
             <div className="w-full lg:w-1/6 relative h-[360px]">
               <Admin_sidebar
