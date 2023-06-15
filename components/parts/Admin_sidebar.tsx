ca084dac12b5a6a0203d97cf3d2a799d1f6e6604
Aryansavaj <126572722+Aryansavaj@users.noreply.github.com>
2023-06-15 17:06:25 +0530

    HJBJGH

--- components/parts/Admin_sidebar.tsx
+++ components/parts/Admin_sidebar.tsx
@@ -9,95 +9,16 @@ import { useRouter } from "next/router";
 import en from "@/locales/en";
 import sv from "@/locales/sv";
 
-type Props = {
-  openSidebar: boolean;
-  setOpenSidebar: (value: boolean) => void;
-};
+type Props = {};
 
-const AuthButton = ({
-  changeLanguage,
-  locale,
-  t,
-  className,
-}: {
-  changeLanguage: any;
-  locale: any;
-  t: any;
-  className: string;
-}) => (
-  <div
-    className={`items-center justify-center lg:justify-end gap-2 mt-8 lg:mt-0 ${className}`}
-  >
-    <div className="relative inline-flex">
-      <svg
-        className="w-2 h-2 absolute top-0 right-0 m-4 pointer-events-none"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 412 232"
-      >
-        <path
-          d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z"
-          fill="#648299"
-          fillRule="nonzero"
-        />
-      </svg>
-      <select
-        onChange={changeLanguage}
-        defaultValue={"sv"}
-        className="border bg-base-100 border-gray-300 rounded-xl h-10 pl-5 pr-10 font-semibold cursor-pointer hover:border-gray-400 focus:outline-none appearance-none"
-      >
-        <option className="font-semibold" value="sv">
-          SV
-        </option>
-        <option className="font-semibold" value="en">
-          EN
-        </option>
-      </select>
-    </div>
-
-    <Link className="btn btn-link" href="/signin">
-      {t.headerData.login}
-    </Link>
-    <Link className="btn btn-primary btn-shadow" href="/signup">
-      {t.headerData.getStarted}
-    </Link>
-    <Link className="btn btn-primary btn-shadow" href="/dashboard">
-      {t.headerData.dashboard}
-    </Link>
-  </div>
-);
-
-export default function Admin_sidebar({ openSidebar, setOpenSidebar }: Props) {
-  const [openMenu, setOpenMenu] = useState(false);
+export default function Admin_sidebar({}: Props) {
+  
 
   const router = useRouter();
-  const { locale, pathname } = router;
+  const { locale } = router;
   const t = locale === "en" ? en : sv;
 
-  const setDefaultLangToSV = () => {
-    router.push(router.pathname, router.asPath, { locale: "sv" });
-  };
-
-  useEffect(() => {
-    return () => {
-      setOpenMenu(false);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (pathname === "/") {
-      setDefaultLangToSV();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const changeLanguage = (e: { target: { value: any } }) => {
-    const locale = e.target.value;
-    router.push(router.pathname, router.asPath, { locale });
-  };
-
-  const toggleMenu = () => {
-    setOpenMenu(!openMenu);
-  };
+  
 
   return (
     <>
