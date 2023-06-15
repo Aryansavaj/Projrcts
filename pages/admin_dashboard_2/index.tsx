0b5b8cebf7732f226a97b0ce9f0efc8414ed4458
Aryansavaj <126572722+Aryansavaj@users.noreply.github.com>
2023-06-15 16:16:42 +0530

    fevd

--- pages/admin_dashboard_2/index.tsx
+++ pages/admin_dashboard_2/index.tsx
@@ -34,9 +34,12 @@ export default function Profile_setting_pop({}: Props) {
   };
 
   const adminItems = [
+    { title: 'Checkified - Key To Success', count: 250, date: 'Jan 7, 2023', imageUrl: '/images/../card_list4.png' },
     { title: 'Checkified - Nyckeln till Framgång', count: 250, date: 'Jan 7, 2023', imageUrl: '/images/../card_list4.png' },
+    { title: 'Checkified - Key To Success', count: 250, date: 'Jan 7, 2023', imageUrl: '/images/../card_list4.png' },
     { title: 'Checkified - Nyckeln till Framgång', count: 250, date: 'Jan 7, 2023', imageUrl: '/images/../card_list4.png' },
-    { title: 'Checkified - Nyckeln till Framgång', count: 250, date: 'Jan 7, 2023', imageUrl: '/images/../card_list4.png' },
+    { title: 'Checkified - Key To Success', count: 250, date: 'Jan 7, 2023', imageUrl: '/images/../card_list4.png' },
+    
   ];
   return (
     <Admin_layout headTitle={profile_pop.metaData.title}>
