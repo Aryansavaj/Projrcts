3679449395e546639d00ee2f5ef9fdee31876d08
Aryansavaj <126572722+Aryansavaj@users.noreply.github.com>
2023-06-15 11:48:14 +0530

    acw

--- components/parts/Admin_list.tsx
+++ components/parts/Admin_list.tsx
@@ -11,20 +11,22 @@ interface AdminListProps {
 
 const AdminList: React.FC<AdminListProps> = ({ title, count, date, imageUrl }) => {
   return (
-    <div className="admin_list flex justify-between items-center">
-      <div className="admin_list_content flex justify-between items-center">
-        <div className="admin_list_img flex justify-between items-center">
-          <img src={imageUrl} alt="Admin Image" />
-        </div>
-        <div className="admin_list_detail">
-          <h4>{title}</h4>
-          <div className="admin_list_extra flex justify-between items-center">
-            <div className="admin_list_count">{count}</div>
-            <div className="admin_list_date">{date}</div>
+    <div className="bg-base-200 p-6">
+      <div className="admin_list flex justify-between items-center bg-white my-4 rounded-[26px]">
+        <div className="admin_list_content flex justify-between items-center">
+          <div className="admin_list_img flex justify-between items-center">
+            <img src={imageUrl} alt="Admin Image" />
+          </div>
+          <div className="admin_list_detail">
+            <h4>{title}</h4>
+            <div className="admin_list_extra flex justify-between items-center">
+              <div className="admin_list_count">{count}</div>
+              <div className="admin_list_date">{date}</div>
+            </div>
           </div>
         </div>
+        <div className="admin_list_menu"><BsThreeDotsVertical/></div>
       </div>
-      <div className="admin_list_menu"><BsThreeDotsVertical/></div>
     </div>
   );
 };
