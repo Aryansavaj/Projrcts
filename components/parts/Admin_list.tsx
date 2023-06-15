ca96e47421ad550c13e2da3419d73206ea7484fe
Aryansavaj <126572722+Aryansavaj@users.noreply.github.com>
2023-06-15 11:48:46 +0530

    ea

--- components/parts/Admin_list.tsx
+++ components/parts/Admin_list.tsx
@@ -11,7 +11,7 @@ interface AdminListProps {
 
 const AdminList: React.FC<AdminListProps> = ({ title, count, date, imageUrl }) => {
   return (
-    <div className="bg-base-200 p-6">
+    <div className="bg-base-200 p-6 rounded-[24px]">
       <div className="admin_list flex justify-between items-center bg-white my-4 rounded-[26px]">
         <div className="admin_list_content flex justify-between items-center">
           <div className="admin_list_img flex justify-between items-center">
