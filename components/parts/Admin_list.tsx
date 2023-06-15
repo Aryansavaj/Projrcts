a45c12caac08d292ea45176375c7d62143b33706
Aryansavaj <126572722+Aryansavaj@users.noreply.github.com>
2023-06-15 14:09:27 +0530

    sdv

--- components/parts/Admin_list.tsx
+++ components/parts/Admin_list.tsx
@@ -20,13 +20,13 @@ const AdminList: React.FC<AdminListProps> = ({ title, count, date, imageUrl }) =
           <div className="admin_list_detail">
             <h4 className="text-base form-text-color font-semibold">{title}</h4>
             <div className="admin_list_extra flex gap-4 items-center">
-              <div className="admin_list_count font-medium text-[10px] text-707BA0 flex items-center">
+              <div className="admin_list_count font-medium text-[10px] text-707BA0 flex items-center bg-EDF5FE">
               <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M8.10666 8.03768C8.03999 8.03102 7.95999 8.03102 7.88666 8.03768C6.29999 7.98435 5.03999 6.68435 5.03999 5.08435C5.03999 3.45102 6.35999 2.12435 7.99999 2.12435C9.63333 2.12435 10.96 3.45102 10.96 5.08435C10.9533 6.68435 9.69333 7.98435 8.10666 8.03768Z" stroke="#707BA0" stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round"/>
                   <path d="M4.77334 10.4977C3.16001 11.5777 3.16001 13.3377 4.77334 14.411C6.60667 15.6377 9.61334 15.6377 11.4467 14.411C13.06 13.331 13.06 11.571 11.4467 10.4977C9.62001 9.27768 6.61334 9.27768 4.77334 10.4977Z" stroke="#707BA0" stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round"/>
                   </svg>&nbsp;
                 {count}</div>
-              <div className="admin_list_date font-medium text-[10px] text-707BA0 flex items-center">
+              <div className="admin_list_date font-medium text-[10px] text-707BA0 flex items-center bg-EDF5FE">
               <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M5.33333 2.12435V4.12435" stroke="#707BA0" stroke-width="1.3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
                 <path d="M10.6667 2.12435V4.12435" stroke="#707BA0" stroke-width="1.3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
