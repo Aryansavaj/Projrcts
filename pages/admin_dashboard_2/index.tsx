ff872d56563cf846ecdda4637c70e40c96fb6714
Aryansavaj <126572722+Aryansavaj@users.noreply.github.com>
2023-06-15 14:00:06 +0530

    cas

--- pages/admin_dashboard_2/index.tsx
+++ pages/admin_dashboard_2/index.tsx
@@ -83,8 +83,8 @@ export default function Profile_setting_pop({}: Props) {
 
                           </div>
                       </div>
-                      <div className="admin2_add_btn text-sm font-semibold text-white bg-0083FF flex items-center">
-                        <button className="btn btn-primary w-full lg:w-fit mb-8 ml-2 mr-2 text-sm font-semibold pt-0 pb-0 btn-height-rem">
+                      <div className="admin2_add_btn text-sm font-semibold text-white flex items-center">
+                        <button className="btn btn-primary text-sm font-semibold ">
                           <AiOutlinePlus/> Add Course
                           </button>
                       </div>
